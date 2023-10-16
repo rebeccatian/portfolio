50ef5cb298ac960b20a0b5c02503cdcd86293e69
rebeccatian805 <rebeccatian@hermanmiller.com>
2023-10-16 15:07:53 -0400

    updated portfolio with github links + more resume updates

--- src/data.ts
+++ src/data.ts
@@ -14,6 +14,8 @@ export interface CardTypes {
     description: string;
     link: string;
     link2?: string;
+    githubLink?: string;
+    githubLink2?: string;
 }
 
 export const cards: CardTypes[] = [
@@ -23,24 +25,30 @@ export const cards: CardTypes[] = [
         image2: seatGeekPic, 
         subtitle: "Next.js, React, TypeScript, Tailwind, Prisma, PostgresSQL", 
         description: "IN PROGRESS / Current version: can check lowest price, save events to your email, and check your saved events through email. Still working on adding email notifications.", 
-        link:"http://ticket-tracker.rebeccatian.dev/"
+        link:"http://ticket-tracker.rebeccatian.dev/",
+        githubLink:"https://github.com/rebeccatian/ticket-tracker"
     },
     {
         category:["frontend"], title: "Mini Games", image2: gamePic, subtitle: "JavaScript, HTML/CSS", 
-        description: "Jan 2022 (8-Ball) & June 2023 (Rock, Paper, Scissors)/ A collection of minigames built purely with vanilla JavaScript ", link:"https://rebeccatian.github.io/eight-ball", link2: "https://rebeccatian.github.io/rock-paper-scissors/"
+        description: "Jan 2022 (8-Ball) & June 2023 (Rock, Paper, Scissors)/ A collection of minigames built purely with vanilla JavaScript ", 
+        link:"https://rebeccatian.github.io/eight-ball", link2: "https://rebeccatian.github.io/rock-paper-scissors/",
+        githubLink:"https://github.com/rebeccatian/eight-ball", githubLink2: "https://github.com/rebeccatian/rock-paper-scissors"
     },
     {
         category:["frontend"], title: "Note App", image2: notePic, 
         subtitle: "React.js, HTML/CSS, JavaScript", 
-        description: "Jan 2022 / A simple Note App with abilites to create, add, edit, and delete.", link:"https://rebeccatian.github.io/my-app/"
+        description: "Jan 2022 / A simple Note App with abilites to create, add, edit, and delete.", link:"https://rebeccatian.github.io/my-app/",
+        githubLink: "https://github.com/rebeccatian/my-app"
     },
     {
         category:["frontend"], title: "Portfolio (this site!)", image2: portPic, subtitle: "React, TypeScript, Tailwind",
-        description: "September 2023 / New portfolio site made to host projects and act as an introduction to me", link:"https://rebeccatian.dev"
+        description: "September 2023 / New portfolio site made to host projects and act as an introduction to me", link:"https://rebeccatian.dev",
+        githubLink: "https://github.com/rebeccatian/portfolio"
     },
     {
         category:["mobile"], title: "Anti-Boredom App", image2: boredomAppPic, subtitle: "Swift", 
-        description: "Nov 2022 / Mobile app that ultilizes API calls to randomly generate an activity for the user", link:"https://github.com/rebeccatian/anti-boredom-app"},
+        description: "Nov 2022 / Mobile app that ultilizes API calls to randomly generate an activity for the user", link:"https://github.com/rebeccatian/anti-boredom-app"
+    },
     {
         category:["mobile", "backend"], title: "Bite Butler", image2: biteButler, subtitle: "React Native, Python, Firebase", 
         description: "Hackathon 2023 Project / Mobile app that combines various restaurant ratings into one average rating and rank them accordingly", link:"https://github.com/rebeccatian/bite-butler"},
